943a1cfcc72c75366dfa3d7b781ce4b5cc9e51ed
Nicolas Castro Giraldo <nicolas.castro9219@gmail.com>
2025-10-01 21:44:15 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'card',
+    loadComponent: () => import('./card/card/card').then((c) => c.Card),
+  },
+  {
+    path:'**',
+    redirectTo:'card'
+  }
+];
